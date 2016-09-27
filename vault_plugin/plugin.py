08584ff8630d32b89d6b472680c3475c65d3a5c0
RcRonco <cohenronco@gmail.com>
2016-09-27 22:04:36 +0300

    Vault plugin for Netflix Lemur.

---
+++ vault_plugin/plugin.py
@@ -0,0 +1,31 @@
+import vault_plugin
+import requests
+
+from lemur.plugins.base.issuer import IssuerPlugin
+
+def process_options(options)
+    raise NotImplemented
+
+class PluginName(IssuerPlugin):
+    title = 'Hashicorp Vault'
+    slug = 'HashicorpVault'
+    description = 'A plugin for hashicorp Vault secret management software.'
+    version = vault_plugin.VERSION
+
+    author = 'Ron Cohen'
+    author_url = 'https://github.com/RcRonco/vault_plugin'
+
+    VAULT_URL = 'https://vault.mydomain.com:8200/pki/sign/cert_role'
+
+   def create_certificate(self, csr, options):
+       post_params = process_options(options)
+       post_params['csr'] = csr
+       resp = requests.post(VAULT_URL, post_params)
+       if resp.status_code != 201:
+            raise ApiError('POST /pki/sign/ '.format(resp.status_code))
+
+     @staticmethod
+    def create_authority(options):
+        raise NotImplemented
+
+       
