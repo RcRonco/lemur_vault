0e448c95858c77416a1d2f5fa1e21a97fdea1164
RcRonco <roncohen04@gmail.com>
2016-10-11 16:49:05 +0300

    Hashicorp Vault pki plugin for Netflix Lemur Certificate Manager.

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Basic package information."""
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+install_requires = [
+    'lemur',
+]
+
+setup(
+    name='lemur_vault',
+    version='0.1',
+    author='Ron Cohen',
+    author_email='roncohen04[at]gmail[dot]com',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'lemur.plugins': [
+            'vault_issuer = lemur_digicert.plugin:DigiCertIssuerPlugin',
+        ]
+    }
+)
--- vault_plugin/plugin.py
+++ vault_plugin/plugin.py
@@ -1,11 +1,19 @@
 import vault_plugin
 import requests
 
-from lemur.plugins.base.issuer import IssuerPlugin
+from lemur.plugins.bases.issuer import IssuerPlugin
+VAULT_ISSUE_PATH = '/v1/pki/issue/cert_role'
 
-def process_options(options)
-    data['format'] = 'pem'
+
+def process_options(options):
+    data = '{"format":"pem", "common_name": "' + options['common_name'] + '"'
+    if options['alt_names']:
+        data += ', "alt_names": "' + options['alt_names'] + '"'
+    if options['ip_sans']:
+        data += ', "ip_sans": "' + options['ip_sans'] + '"'
+    data += '}'
     return data
+
 
 class PluginName(IssuerPlugin):
     title = 'Hashicorp Vault'
@@ -16,20 +24,38 @@ class PluginName(IssuerPlugin):
     author = 'Ron Cohen'
     author_url = 'https://github.com/RcRonco/vault_plugin'
 
-    VAULT_URL = 'https://vault.mydomain.com:8200/pki/sign/cert_role'
+    options = [
+        {
+            'name': 'VaultURL',
+            'type': 'str',
+            'required': True,
+            'validation': '@(https?|http)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$@iS',
+            'helpMessage': 'Must be a valid Vault server URL!',
+        },
+        {
+            'name': 'VaultAuthToken',
+            'type': 'str',
+            'required': True,
+            'validation': '/^$|\s+/',
+            'helpMessage': 'Must be a valid Kubernetes server Token!',
+        },
+    ]
 
-   def create_certificate(self, csr, options):
-       post_params = process_options(options)
-       post_params['csr'] = csr
-       resp = requests.post(VAULT_URL, post_params)
-       
-       # Check for error from vault_plugin
+    def create_certificate(self, options):
+        headers = {'X-Vault-Token': self.get_option('VaultAuthToken', self.options)}
+        url = self.get_option('VaultURL', self.options) + VAULT_ISSUE_PATH
+        params = process_options(options)
+        resp = requests.put(url, data=params, headers=headers)
+        if resp.status_code != 200:
+            raise Exception('vault_error', resp.status_code)
 
-       cert = resp.json['data']['certificate']
-       return cert
+        cert = resp.json['data']['certificate']
 
-     @staticmethod
+        if not cert:
+            return cert
+        else:
+            raise Exception("error")
+
+    @staticmethod
     def create_authority(options):
         raise NotImplemented
-
-       
